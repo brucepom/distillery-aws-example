cb522fbff3ad6476b26e849e509ab5613b568878
Bruce Pomeroy <bruce@brucepomeroy.com>
2019-02-26 08:04:06 -0800

    make test fail

--- test/example_web/views/error_view_test.exs
+++ test/example_web/views/error_view_test.exs
@@ -4,6 +4,10 @@ defmodule ExampleWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  test "make me fail" do
+    assert "make me fail" == 1
+  end
+
   test "renders 404.json" do
     assert render(ExampleWeb.ErrorView, "404.json", []) ==
            %{errors: %{detail: "Not Found"}}
